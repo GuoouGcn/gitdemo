30032360b8b022f66d6dc33ffc28d2e1c5d03d20
v_guojixian <v_guojixian@baidu.com>
2018-06-03 11:37:08 +0800

    初始提交

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Random;
+
+import com.sun.deploy.util.StringUtils;
+
+public class Main {
+
+    public static void main(String[] args) {
+        StringUtils.replaceSeparators("1");
+        System.out.println("Hello World im gjx!");
+    }
+}

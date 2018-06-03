ac562d993e312a80674e3cc87c96db33977ef3ec
v_guojixian <v_guojixian@baidu.com>
2018-06-03 11:43:53 +0800

    分支1修改

--- src/Main.java
+++ src/Main.java
@@ -7,5 +7,6 @@ public class Main {
     public static void main(String[] args) {
         StringUtils.replaceSeparators("1");
         System.out.println("Hello World im gjx!");
+        System.out.println("另一个分支");
     }
 }

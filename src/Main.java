d0aacc9176af341d64eb2d017c4c61e147357507
v_guojixian <v_guojixian@baidu.com>
2018-06-03 11:39:13 +0800

    修改

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,6 @@ public class Main {
 
     public static void main(String[] args) {
         StringUtils.replaceSeparators("1");
-        System.out.println("Hello World im gjx!");
+        System.out.println("Hello World im gj13x!");
     }
 }

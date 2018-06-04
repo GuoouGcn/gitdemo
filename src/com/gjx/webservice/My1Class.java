c0dabff21113e9dce5b6e8c15ff9f1812b775039
v_guojixian <v_guojixian@baidu.com>
2018-06-04 15:40:45 +0800

    昂俺

---
+++ src/com/gjx/webservice/My1Class.java
@@ -0,0 +1,12 @@
+package com.gjx.webservice;
+
+/**
+ * @author: guojixian
+ * @create: 2018-06-04 15:40
+ **/
+public class My1Class {
+
+    public void test2() {
+        System.out.println("aaaa");
+    }
+}
